3f28358517a478cffaaee60f3bfea4fa6e1cd55e
nemerald <robin_wilde@hotmail.com>
2018-09-10 12:08:45 +0200

    Removed unused imports

--- app/src/main/java/com/nemerald/apiproject/FavoriteFragment.java
+++ app/src/main/java/com/nemerald/apiproject/FavoriteFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -16,8 +15,6 @@ import com.nemerald.apiproject.Adapters.RecyclerViewAdapter;
 import com.nemerald.apiproject.Dialogs.ShowPictureDialogFragment;
 import com.nemerald.apiproject.Objects.FavoriteGallery;
 import com.nemerald.apiproject.Objects.Picture;
-
-import java.io.Serializable;
 
 public class FavoriteFragment extends Fragment{
 
